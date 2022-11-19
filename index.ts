c81b607364e8a878a03e6ddc029a39b57888377e
timothystorm <timothystorm@gmail.com>
2022-11-19 08:33:10 -0700

    feat(rxjs): create template

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import './style.css';
 
 import { Observable } from 'rxjs';
 
-const countdownFrom = 100;
+/**
+ * Using RxJs, create a countdown timer that is started or reset 
+ * every time the cursor hovers over the red panel.  Show the countdown
+ * time in the black panel.
+ */
 const timerEl = document.getElementById('countdown');
 const resetEl = document.getElementById('reset');
+timerEl.innerHTML = '100';
